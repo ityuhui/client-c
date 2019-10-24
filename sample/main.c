5a0013980df8bc3dcf6dfb66e746e2701631a44e
Hui Yu <ityuhui@gmail.com>
2019-10-24 13:58:39 +0800

    sample

--- sample/main.c
+++ sample/main.c
@@ -14,7 +14,7 @@ void create_a_pod(apiClient_t *apiClient)
     podinfo->spec = calloc(1, sizeof(v1_pod_spec_t));
 
     podinfo->metadata = calloc(1, sizeof(v1_object_meta_t));
-    podinfo->metadata->name = strdup("test-p1");
+    podinfo->metadata->name = strdup("test-p3");
 
     list_t *containerlist = list_create();
     v1_container_t *con = calloc(1, sizeof(v1_container_t));
@@ -46,21 +46,37 @@ void list_pod(apiClient_t *apiClient)
     CoreV1API_listCoreV1NamespacedPod(apiClient, namesapce, NULL, 0, 0, NULL, NULL, 5, NULL, 30, 0);
 }
 
-int main()
+void print_usage()
 {
+    printf("Usage: main baseurl(mandotory) token(mandotory) cafile(optional)\n\
+e.g. main https://kubernetes:6443 mtpZCI6IjJZT3k1bDNK\n");
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3) {
+        print_usage();
+        return 1;
+    }
+
     list_t *apiKeys;
     apiKeys = list_create();
     
-    //char *keyToken = strdup("Authorization");
-    //char *valueToken = strdup("Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjJZT3k1bDNKTVc3Nk9qWXlqTVlYcXFRZWQwb2tGWnNFNWI0eVY1cnBQNzgifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImRlZmF1bHQtdG9rZW4tOHA0aDQiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiZGVmYXVsdCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6ImMxNjhmM2NjLTFhODQtNGNjNC04ZmUxLWJjZDNhYTE5MTllOCIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0OmRlZmF1bHQifQ.IwgMXmMmTG_bmARroB2S_Fc-7tvrQ4WpyadzXVNVlzLeSuMgABSisBmUfTURwIstOZ_yojJIC6UXYl2Cq2IELl9cJZ6Z1knktvtgfCjqUNUCFqNRkAbt4TB8b22yZEi_ZngvxDknRkJv79XcDm6Nab9MutWMN6b45iH1aL-1W93fqqXburakkpDeMm5fN0TcYxT_zLV7LWjVwWRsvcIEr1dNfVjmZiKdTs9Oepbm__t9q-kvk-t2ItzwPFuNkpygNWsQSH_lDeIutNucBh7oRPUtkvBGDlyOajYcV-Xy1ufSicdw_Mm-oDKgPB72WEYyc87H5Z8SGn6VaCqlTwiDFw");
-    //keyValuePair_t *keyPairToken = keyValuePair_create(keyToken, valueToken);
-    //list_addElement(apiKeys, keyPairToken);
+    char *keyToken = strdup("Authorization");
+    char valueToken[1024];
+    memset(valueToken, 0, sizeof(valueToken));
+    sprintf(valueToken, "Bearer %s", argv[2]);
+
+    keyValuePair_t *keyPairToken = keyValuePair_create(keyToken, valueToken);
+    list_addElement(apiKeys, keyPairToken);
 
     //apiClient_t *app = apiClient_create("https://kubernetes:6443", apiKeys);
 
-    //apiClient_t *apiClient = apiClient_create("https://9.111.254.254:6443", apiKeys, NULL);
+    //For hypervisor
+    apiClient_t *apiClient = apiClient_create("https://9.111.254.254:6443", apiKeys, NULL);
 
-    apiClient_t *apiClient = apiClient_create("http://localhost:8001", NULL, NULL);
+    //For kubectl proxy
+    //apiClient_t *apiClient = apiClient_create("http://localhost:8001", NULL, NULL);
 
     create_a_pod(apiClient);
 
